a5883e06f0e151a60c52166149565a3dadb52172
pobryan <72995425+pobryan@users.noreply.github.com>
2023-04-27 00:11:05 -0700

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -464,3 +464,369 @@ void checkTiles(int board[][8], int whiteDanger[][8], int blackDanger[][8]) {
     }
 
 }
+
+bool checkMate(int board[][8], int whiteDanger[][8], int blackDanger[][8], int color) {
+    int tempBoard[8][8];
+    bool result = false;
+    bool inCheck = true;
+    int KingX, KingY;
+
+    if (color == 2) {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                for (int a = 0; a < 8; a++) {
+                    for (int b = 0; b < 8; b++) {
+
+                        for (int i = 0; i < 8; i++) {
+                            for (int j = 0; j < 8; j++) {
+                                tempBoard[i][j] = board[i][j];
+                            }
+                        }
+
+                        switch (board[x][y]) {
+
+                        case WPawn:
+                            result = movePawn(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == WKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (blackDanger[KingX][KingY] == 0) {
+                                    cout << "Can be saved by (" << x << "," << y << ") to (" << a << "," << b << ")" << endl;
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case WKnight:
+                            result = moveKnight(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == WKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (blackDanger[KingX][KingY] == 0) {
+                                    cout << "Can be saved by (" << x << "," << y << ") to (" << a << "," << b << ")" << endl;
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case WBishop:
+                            result = moveBishop(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == WKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (blackDanger[KingX][KingY] == 0) {
+                                    cout << "Can be saved by (" << x << "," << y << ") to (" << a << "," << b << ")" << endl;
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case WRook:
+                            result = moveRook(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == WKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (blackDanger[KingX][KingY] == 0) {
+                                    cout << "Can be saved by (" << x << "," << y << ") to (" << a << "," << b << ")" << endl;
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case WQueen:
+                            result = moveBishop(x, y, a, b, board);
+                            if (result == false) {
+                                result = moveRook(x, y, a, b, board);
+                            }
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == WKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (blackDanger[KingX][KingY] == 0) {
+                                    cout << "Can be saved by (" << x << "," << y << ") to (" << a << "," << b << ")" << endl;
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case WKing:
+                            result = moveKing(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == WKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (blackDanger[KingX][KingY] == 0) {
+                                    cout << "Can be saved by (" << x << "," << y << ") to (" << a << "," << b << ")" << endl;
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        }
+                    }
+                }
+            }
+            cout << "loop" << endl;
+        }
+        cout << "Checkmate" << endl;
+        return true;
+    }
+    else {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                for (int a = 0; a < 8; a++) {
+                    for (int b = 0; b < 8; b++) {
+
+                        for (int i = 0; i < 8; i++) {
+                            for (int j = 0; j < 8; j++) {
+                                tempBoard[i][j] = board[i][j];
+                            }
+                        }
+
+                        switch (board[x][y]) {
+
+                        case BPawn:
+                            result = movePawn(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == BKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (whiteDanger[KingX][KingY] == 0) {
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case BKnight:
+                            result = moveKnight(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == BKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (whiteDanger[KingX][KingY] == 0) {
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case BBishop:
+                            result = moveBishop(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == BKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (whiteDanger[KingX][KingY] == 0) {
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case BRook:
+                            result = moveRook(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == BKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (whiteDanger[KingX][KingY] == 0) {
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case BQueen:
+                            result = moveBishop(x, y, a, b, board);
+                            if (result == false) {
+                                result = moveRook(x, y, a, b, board);
+                            }
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == BKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (whiteDanger[KingX][KingY] == 0) {
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        case BKing:
+                            result = moveKing(x, y, a, b, board);
+
+                            if (result == true) {
+                                tempBoard[a][b] = tempBoard[x][y];
+                                tempBoard[x][y] = blank;
+
+                                checkTiles(tempBoard, whiteDanger, blackDanger);
+
+                                for (int i = 0; i < 8; i++) {
+                                    for (int j = 0; j < 8; j++) {
+                                        if (tempBoard[i][j] == BKing) {
+                                            KingX = i;
+                                            KingY = j;
+                                        }
+                                    }
+                                }
+
+                                if (whiteDanger[KingX][KingY] == 0) {
+                                    return false;
+                                }
+                            }
+
+                            break;
+
+                        }
+                    }
+                }
+            }
+            cout << "loop" << endl;
+        }
+        cout << "Checkmate" << endl;
+        return true;
+    }
+}
