7c17ae90069aeb574d65df0418baed6187d39e49
Robert Kelly <r_kelly5@u.pacific.edu>
2023-05-05 19:08:24 -0700

    Updated header to add Check and Engine Functions

--- ChessEngineGame/ChessEngineGame/header1.h
+++ ChessEngineGame/ChessEngineGame/header1.h
@@ -15,4 +15,8 @@ bool moveBishop(int x, int y, int a, int b, int board[][8]);
 bool moveRook(int x, int y, int a, int b, int board[][8]);
 bool moveKing(int x, int y, int a, int b, int board[][8]);
 
-//void checkTiles(int board[][8]);
+void checkTiles(int board[][8], int whiteDanger[][8], int blackDanger[][8]);
+bool checkMate(int board[][8], int whiteDanger[][8], int blackDanger[][8], int color);
+
+void legal(int board[][8], int whiteDanger[][8], int blackDanger[][8]);
+int legalJump(int board[][8], int whiteDanger[][8], int blackDanger[][8], int x, int y, int a, int b);
